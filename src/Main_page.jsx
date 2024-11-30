112dd0d3db63d6a75d2b4d65935e02779cf54efd
yash kumar jangir <yashjangirme@gmail.com>
2024-12-01 00:44:24 +0530

    new commit

--- src/Main_page.jsx
+++ src/Main_page.jsx
@@ -6,6 +6,7 @@ function Main_page(){
             <div>
                 <NavBar/>
             </div>
+
             <div className="main_page">
                 <div className="main_text">
                     <div className="up_text">
@@ -15,10 +16,30 @@ function Main_page(){
                         THE BEST
                         </div>  
                 </div>
-            <div>
-                <img src="/images/shoe_image.png" alt="" />
+                <div className="img_si">
+                    <img src="/images/shoe_image.png" alt="" />
+                </div>
             </div>
-        </div>
+
+            <div className="next_page">
+                <div className="next_text">
+                   TOP OF THE WISHLIST KICKS 
+                </div>   
+                <div className="next_text2">
+                    All-time icons and sport-inspired gifts to make everyone on your list look and feel their best
+                </div>
+                <button className="next_btn">
+                    SHOP KICKS
+                </button>
+
+                <div className="nextt">
+                    <div >
+                        <h2>The Latest</h2>
+                    </div>
+                </div>
+                
+            </div>
+          
         </div>
         
     )
