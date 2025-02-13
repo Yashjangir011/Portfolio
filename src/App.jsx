552076b7f6eeefe5ef47db6efe4f5139f32596ef
yash kumar jangir <yashjangirme@gmail.com>
2025-02-14 02:55:03 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import Navbar from './nav_bar'
 import About from './About'
 import Project from './project'
 
 function App() {
   return (
-    <div>
-       <Navbar/>
-       <About/>
-       <Project/>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <About />
+      <Project />
+    </BrowserRouter>
   )
 }
 
